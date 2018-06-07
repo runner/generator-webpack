d59c696a5d6a5581260dfab98861174afdd4d090
Stanislav Kalashnik <s.kalashnik@infomir.com>
2018-06-07 17:08:09 +0300

    move code from spasdk/tasks repo and rework

---
+++ index.js
@@ -0,0 +1,195 @@
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+var name  = 'webpack',
+    tools = require('@runner/tools'),
+    log   = require('@runner/logger').wrap(name);
+
+
+function report ( config, instance, error, stats ) {
+    var path = require('path'),
+        dir  = path.relative('.', config.output.path);
+
+    if ( error ) {
+        log.fail(error.toString());
+    } else {
+        instance.stats = stats.toJson({source: false});
+
+        log.info('time: %s ms', log.colors.magenta(instance.stats.time));
+        log.info('hash: ' + log.colors.grey(instance.stats.hash));
+
+        instance.stats.assets.forEach(function ( asset ) {
+            log.info(
+                'write %s (size: %s)',
+                log.colors.bold(path.join(dir, asset.name)),
+                log.colors.green(asset.size)
+            );
+        });
+
+        instance.stats.errors.forEach(function ( error ) {
+            error = error.split('\n');
+            log.fail('%s %s', log.colors.bold(error.shift()), error.shift());
+            console.log(log.colors.red(error.join('\n')));
+        });
+
+        instance.stats.warnings.forEach(function ( warning ) {
+            warning = warning.split('\n');
+            log.warn('%s %s', log.colors.bold(warning.shift()), warning.shift());
+            console.log(log.colors.yellow(warning.join('\n')));
+        });
+    }
+}
+
+
+function watch ( config, instance, done ) {
+    var webpack = require('webpack');
+
+    // reuse existing instance if possible
+    instance.compiler = instance.compiler || webpack(config);
+
+    if ( instance.compiler.running ) {
+        // if build function is still running
+        log.fail('You ran Webpack twice. Each instance only supports a single concurrent compilation at a time.');
+        done();
+    } else {
+        instance.watcher = instance.compiler.watch(config.watchOptions, function ( error, stats ) {
+            report(config, instance, error, stats);
+            if ( instance.buildInWatch ) {
+                // if the watch function is called by the build function
+                instance.buildInWatch = false;
+                done();
+            }
+        });
+
+        if ( !instance.buildInWatch ) {
+            instance.watcherDone = done;
+        }
+    }
+
+    return instance;
+}
+
+
+function build ( config, instance, done ) {
+    var webpack = require('webpack');
+
+    // reuse existing instance if possible
+    instance.compiler = instance.compiler || webpack(config);
+
+    if ( instance.watcher ) {
+        // if watch function is still running
+        instance.buildInWatch = true;
+        instance.watcher.close(function () {
+            watch(config, instance, done);
+        });
+    } else {
+        instance.compiler.run(function ( error, stats ) {
+            report(config, instance, error, stats);
+            done();
+        });
+    }
+
+    return instance;
+}
+
+
+function unwatch ( instance ) {
+    if ( instance.watcher ) {
+        instance.watcher.close(function () {
+            instance.watcher = null;
+            instance.buildInWatch = false;
+            instance.watcherDone();
+        });
+    }
+
+    return instance;
+}
+
+
+function clear ( config, done ) {
+    var path  = require('path'),
+        files = [path.relative('.', path.join(config.output.path, config.output.filename))];
+
+    // add map file
+    if ( config.output.sourceMapFilename ) {
+        files.push(path.relative('.', path.join(config.output.path, config.output.sourceMapFilename)));
+    }
+
+    tools.unlink(files, log, done);
+}
+
+
+function modules ( instance ) {
+    if ( instance ) {
+        instance.stats.modules.forEach(function ( module ) {
+            log.info(log.colors.bold(module.name));
+            if ( module.reasons.length ) {
+                module.reasons.forEach(function ( reason ) {
+                    log.info('    %s %s from %s',
+                        log.colors.grey(reason.type),
+                        log.colors.green(reason.userRequest),
+                        log.colors.green(reason.module)
+                    );
+                });
+            } else {
+                log.info(log.colors.grey('    (root)'));
+            }
+        });
+    }
+}
+
+
+function generator ( config, options ) {
+    var tasks    = {},
+        instance = {};
+
+    // sanitize and extend defaults
+    options = Object.assign(generator.options, options || {});
+
+    tasks[options.prefix + 'config' + options.suffix] = function () {
+        log.inspect(config);
+    };
+
+    tasks[options.prefix + 'build' + options.suffix] = function ( done ) {
+        instance = build(config, instance, done);
+    };
+
+    tasks[options.prefix + 'modules' + options.suffix] = function () {
+        modules(instance);
+    };
+
+    tasks[options.prefix + 'clear' + options.suffix] = function ( done ) {
+        clear(config, done);
+    };
+
+    tasks[options.prefix + 'watch' + options.suffix] = function ( done ) {
+        instance = watch(config, instance, done);
+    };
+
+    tasks[options.prefix + 'unwatch' + options.suffix] = function () {
+        instance = unwatch(instance);
+    };
+
+    return tasks;
+}
+
+
+// defaults
+generator.options = {
+    prefix: name + ':',
+    suffix: ''
+};
+
+
+// export main actions
+generator.methods = {
+    build: build
+};
+
+
+// public
+module.exports = generator;
---
+++ readme.md
@@ -0,0 +1,100 @@
+Tasks generator for Webpack
+===========================
+
+[![build status](https://img.shields.io/travis/runner/generator-webpack.svg?style=flat-square)](https://travis-ci.org/runner/generator-webpack)
+[![npm version](https://img.shields.io/npm/v/@runner/generator-webpack.svg?style=flat-square)](https://www.npmjs.com/package/@runner/generator-webpack)
+[![dependencies status](https://img.shields.io/david/runner/generator-webpack.svg?style=flat-square)](https://david-dm.org/runner/generator-webpack)
+[![devDependencies status](https://img.shields.io/david/dev/runner/generator-webpack.svg?style=flat-square)](https://david-dm.org/runner/generator-webpack?type=dev)
+[![Gitter](https://img.shields.io/badge/gitter-join%20chat-blue.svg?style=flat-square)](https://gitter.im/DarkPark/runner)
+[![RunKit](https://img.shields.io/badge/RunKit-try-yellow.svg?style=flat-square)](https://npm.runkit.com/@runner/generator-webpack)
+
+
+## Installation ##
+
+```bash
+npm install @runner/generator-webpack
+```
+
+
+## Usage ##
+
+Add to the scope:
+
+```js
+var generator = require('@runner/generator-webpack');
+```
+
+Generate tasks according to the given config:
+
+```js
+var tasks = generator({
+    mode: 'development'
+});
+```
+
+Add generated tasks to the `runner` instance:
+
+```js
+var runner = require('@runner/core');
+
+Object.assign(runner.tasks, tasks);
+```
+
+The following tasks will become available:
+
+ Task name         | Description
+-------------------|-------------
+ `webpack:config`  | prints the current configuration used for generated tasks
+ `webpack:build`   | performs webpack compilation 
+ `webpack:modules` | prints detailed info on files used in the latest compilation
+ `webpack:clear`   | removes compiled file (source map as well)
+ `webpack:watch`   | starts file changes monitoring and rebuilds when necessary
+ `webpack:unwatch` | stops monitoring
+
+Generator accepts two arguments: base configuration and additional options.
+
+
+### Base configuration ###
+
+It's a Webpack [config](https://webpack.js.org/configuration/) passed to the [compiller instance](https://webpack.js.org/api/node/#compiler-instance).
+
+
+### Additional options ###
+
+It's an object with the following properties:
+
+ Name   | Description
+--------|-------------
+ prefix | an affix placed before a task name (default is `webpack:`)  
+ suffix | a string added at the end of a task name (empty by default)
+ 
+So it's possible to change generated tasks names: 
+
+```js
+Object.assign(runner.tasks,
+    generator(config, {
+        prefix: 'js:',
+        suffix: ':develop'
+    })
+);
+```
+
+It will add the following tasks:
+
+`js:config:develop` 
+`js:build:develop`  
+`js:modules:develop`
+`js:clear:develop`  
+`js:watch:develop`
+`js:unwatch:develop`
+ 
+
+## Contribution ##
+
+If you have any problems or suggestions please open an [issue](https://github.com/runner/generator-webpack/issues)
+according to the contribution [rules](.github/contributing.md).
+
+
+## License ##
+
+`@runner/generator-webpack` is released under the [GPL-3.0 License](http://opensource.org/licenses/GPL-3.0).
